0a46e481e1104068dc8915f39b4e15dfd55fe81f
Amir <amir.moravej@gmail.com>
2014-05-20 00:34:26 -0400

    issue #10

---
+++ models.js
@@ -0,0 +1,323 @@
+var self = this,
+	mongoose = require('mongoose'),
+	redisClient,
+	dashes_ids = [ 
+		'dash:NTJhOWRmMGYxODNiNTAwMDAwMDAwMDAx',
+		'dash:NTI5NjNiMGYwZWZhNzI1ZTliMDAwMDAx',
+		'dash:NTI5NjJmYzUyMzQ3ZjUxZDliMDAwMDAx',
+		'dash:NTI5NjM5ZTlmOGZjY2Q1ODliMDAwMDAx',
+		'dash:NTI5NjM4NTg2NGZmOGU0YzliMDAwMDAx',
+		'dash:NTI5NjNhNjFmNTliN2Y1YzliMDAwMDAx',
+		'dash:NTI5NjMyNWY1OGM5YmIzNzliMDAwMDAx',
+		'dash:NTI5NjM2YmZiMTcxMzg0NTliMDAwMDAx',
+		'dash:NTI5NjJmYzUyNzQ3ZjUxADLiQDBzMDAy'   ],
+	collections = {
+		'PopularDribbleShot':  '',
+		'EveryoneDribbleShot': '',
+		'DebutsDribbleShot': '',
+		'BusinessInsiderFeed': '' ,
+		'WiredFeed': '',
+		'VentureBeatFeed': '',
+		'TechCrunchFeed': '',
+		'NewYorkTimesFeed':'',
+		'MashableFeed':'',
+		'IncComFeed':'',
+		'ForbesFeed':'',
+		'FastCompanyFeed': '', 
+		'ESPNFeed': '', 
+		'Core77Feed': '', 
+		'DesignMilkFeed': '', 
+		'SideBarIOFeed': '',
+		'BehanceSearchResult': '',
+		'DribbbleStat': '', 
+		'DribbblePlayer': '',
+		'DribbbleShot':'',
+		'PlacesSearchResult': '',
+		'Behance': ''
+	};
+
+var UserSchema = new mongoose.Schema({
+	id: String,
+	uuid: String,
+	dashes: String,
+	created_at: Number,
+});
+
+var User = mongoose.model('User', UserSchema);
+
+var UserSessionSchema = new mongoose.Schema({
+	id: String,
+	uuid: String,
+	is_active: { type: Boolean, default: true },
+	is_defective: { type: Boolean, default: false },
+	begin_time: Date,
+	end_time: Number,
+	duration: Number,
+	update_times: [],
+	locations: [],
+	clicks: [],
+	terms: []
+});
+
+UserSessionSchema.methods.createFromCache = function(session) {
+	
+};
+
+var UserSession = mongoose.model('UserSession', UserSessionSchema);
+
+var DashSchema = new mongoose.Schema({
+	id: {type: String, required: true, unique: true},
+	title: {type: String, required: true},
+	uri: String,
+	dash_type: String,
+	description: String,
+	credits: String,
+	icon_large: String,
+	icon_small: String,
+	setting_type: String,
+	settings: {}
+});
+
+var UserDashSchema = new mongoose.Schema({
+	id: { type: String, required: true, unique: true },
+	dash_id: String,
+	uuid: { type: String, required: true },
+	title: { type: String, required: true },
+	dash_type: String,
+	location: String,
+	uri: String,
+	description: String,
+	credits: String,
+	icon_large: String,
+	icon_small: String,
+	setting_type: String,
+	selected_setting: String,
+	is_active: {type: Boolean, default: true},
+	settings: {}
+});
+
+var UserDash = mongoose.model('UserDash', UserDashSchema);
+
+var PrivateDashSchema = new mongoose.Schema({
+
+	id: String,
+	confirmation_id: String,
+	
+	dash_type: { type: String, default: 'privateDash'},
+	api_end_point: String,
+	dash_title: String,
+  	type_indicator: String,
+  	header: String,
+  	text: String,
+  	main_img: String,
+  	footer: String,
+  	image_key: String,
+  	container: String,
+  	footer_key: String,
+
+	user_id: String,
+	user_name: String,
+	user_email: String,
+
+	view_count: { type: Number, default: 0 },
+	add_count: { type: Number, default: 0 },
+
+	confirmed: { type: Boolean, default: false },
+	created_at: Date,
+	confirmed_at: Date
+});
+
+PrivateDashSchema.methods.json = function(pass) {
+	var u = this.toObject();
+	delete u['_id'];
+	delete u['__v'];
+	delete u['confirmation_id'];
+	delete u['user_name'];
+	delete u['user_email'];
+	delete u['user_id'];
+	return u;
+};
+
+PrivateDashSchema.set('toObject', { virtuals: true });
+
+var PrivateDash = mongoose.model('PrivateDash', PrivateDashSchema);
+
+var ContentSchema = new mongoose.Schema({
+	id: String,
+	source_id: String,
+	content_type: String,
+	term: String,
+	slideshow: {},
+	news: {},
+	stats: {},
+	user_profile: {},
+	content: {},
+	geo: {},
+	collection_name: String
+});
+
+PrivateDashSchema.set('toObject', { virtuals: true });
+
+var Content = mongoose.model('', ContentSchema);
+
+function _objectId() {
+	var id = (new mongoose.Types.ObjectId).toString();
+	id = crypto.createHash('sha1').update(id).digest('hex');
+	return (new Buffer(id).toString('base64').replace(/=/g, ""));
+};
+
+function config (m, r) {
+	redisClient = r;
+	return self;
+};
+
+function findOneDash(id, callback) {
+	redisClient.hgetall('dash:'+id, function(error, _dash){
+
+		if (error) throw error;
+
+		if (_dash.setting_type == 'radio') {
+			_dash.settings = _dash.settings.split(":");
+		}
+
+		callback(null, _dash);
+
+	});
+};
+
+function findDash(callback) {
+
+	var d = [];
+
+	for (var i = 0; i < dashes_ids.length; ++i) {
+		
+		// redisClient.del(dashes_ids[i]);
+
+		(function(i){
+			
+			redisClient.hgetall(dashes_ids[i], function(error, _dash) {
+				
+				if (error) throw error;
+
+				if (_dash.setting_type == 'radio') {
+					_dash.settings = _dash.settings.split(":");
+				}
+
+				d.push(_dash);
+
+				if (i == (dashes_ids.length - 1)) callback(null, d);
+
+			});
+
+		}(i));
+	}
+};
+
+function createUser(user) {
+
+	redisClient.hmset('user:'+user.uuid ,{
+		uuid: user.uuid,
+		dashes: user.dashes,
+		created_at: user.created_at
+	});
+
+	user.is_active = true;
+	user.is_defective = false;
+	user.begin_time = user.created_at;
+	user.end_time = '';
+	user.duration = '';
+	user.update_times = '';
+	user.locations = 'lat:'+user.lat+'|lon:'+user.lat;
+	user.clicks = '';
+	user.terms = '';
+
+	createSession(user)
+};
+
+function createSession(session) {
+	redisClient.hmset('session:'+session.uuid, {
+		uuid: session.uuid,
+		created_at: session.created_at,
+		is_active: session.is_active || true,
+		is_defective: session.is_defective || false,
+		begin_time: session.begin_time || new Date().getTime().toString(),
+		end_time: '',
+		duration: '',
+		update_times: '',
+		locations: 'lat:'+session.lat+'lon:'+session.lon,
+		clicks: '',
+		terms: '',
+	});
+
+};
+
+function deleteSession(uuid) {
+	redisClient.del('session:'+uuid);
+};
+
+function createOrUpdateSession(options) {
+	
+	redisClient.hgetall('session:'+options.uuid, function(error, session){
+		
+		if (error) return callback(error);
+
+		if (!session || session.length == 0) 
+			return createSession(options, callback);
+
+		session[0].is_active = false;
+		session[0].is_defective = true;
+		session[0].end_time = new Date().getTime().toString();
+
+		UserSession.createFromCache(session[0]);
+
+		session[0].is_active = true;
+		session[0].is_defective = false;
+		session[0].begin_time = options.created_at;
+		session[0].end_time = '';
+
+		createSession(options);
+	});
+};
+
+function cipher (text) {
+	// change the key
+	var key = 'NmU5MTgzYzJhNjM1N2JkZjhhMjAxZDc5OWM0ODFlZDYzMTYxNmQ3Ng';
+
+	var cipher = crypto.createCipher('aes-256-cbc', key);
+	var crypted = cipher.update(text, 'utf8', 'hex');
+	crypted += cipher.final('hex');
+	return crypted;		
+};
+
+function decipher(text){
+	var key = 'NmU5MTgzYzJhNjM1N2JkZjhhMjAxZDc5OWM0ODFlZDYzMTYxNmQ3Ng';
+	var decipher = crypto.createDecipher('aes-256-cbc', key);
+	var dec = decipher.update(text,'hex','utf8');
+	dec += decipher.final('utf8');
+	return dec;
+};
+
+var Dash = {
+	findOne: findOneDash,
+	find: findDash
+};
+
+var User = {
+	create: createUser
+};
+
+var Session = {
+	create: createSession,
+	createOrUpdate: createOrUpdateSession,
+	deleteSession: deleteSession
+};
+
+exports.config = config;
+exports.id = _objectId;
+exports.User = User;
+exports.UserSession = UserSession;
+exports.UserDash = UserDash;
+exports.Dash = Dash;
+exports.PrivateDash = PrivateDash;
+exports.Content = Content;
+exports.Session = Session;
