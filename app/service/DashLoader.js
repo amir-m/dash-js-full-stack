c7ce71046ef106ba04f0fae4e30afe033f274cd7
Dev <amir.moravej@gmail.com>
2014-05-31 16:43:05 -0400

    latest

--- app/service/DashLoader.js
+++ app/service/DashLoader.js
@@ -16,6 +16,8 @@ angular.module('DashbookApp')
           $rootScope.user = data.user;
           $rootScope.myDashes = data.dashes;
 
+          console.log($rootScope.user);
+
           if ($rootScope.user.status != 3 || $rootScope.user.status != '3') 
             return $location.path('/register');
 
