bc4c11c914590bfd269cc28bc55b854eee345fd1
Dev <amir.moravej@gmail.com>
2014-06-03 16:19:55 -0400

    latest

--- config.js
+++ config.js
@@ -157,4 +157,4 @@ module.exports = function(express, app, mongoose, cookie, models, redisClient) {
 };
 
 
-// hmset user:NEYxOEU4NjctMjQzOS00NzMzLUI0QzgtQjE4N0QxNEQzNDU3 created_at 1400561882934 uuid NEYxOEU4NjctMjQzOS00NzMzLUI0QzgtQjE4N0QxNEQzNDU3 status 1
+// hmset user:NEYxOEU4NjctMjQzOS00NzMzLUI0QzgtQjE4N0QxNEQzNDU3 created_at 1400561882934 uuid NEYxOEU4NjctMjQzOS00NzMzLUI0QzgtQjE4N0QxNEQzNDU3 status 1 dashes ''
