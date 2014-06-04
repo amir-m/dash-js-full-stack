f7c52883bc6612ab441f957b1e3ff2682a67c5eb
Dev <amir.moravej@gmail.com>
2014-06-04 15:48:53 -0400

    latest

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ setTimeout(function() {
 
 	// require('./helpers')(models, publisher).insertDashesToRedisBackend();
 	// require('./helpers')(models, publisher).deleteAllUsers();
-	require('./helpers')(models, publisher).fixSomething();
+	// require('./helpers')(models, publisher).fixSomething();
+	
 }, 3000);
 
 require('./config')(express, app, mongoose, cookie, models, publisher);
