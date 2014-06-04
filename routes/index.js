5297f837a50005862b677d7476695c492c1079a1
Dev <amir.moravej@gmail.com>
2014-06-04 17:02:08 -0400

    latest

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ module.exports = function (models, publisher, cookie) {
 		}
 		if (code && code == 'notmanparty') {
 			setTimeout(function(){
-				require('../helpers')(models, publisher).confirmUser(req.body.email, 'Amir', function(error){
+				require('../helpers')(models, publisher).confirmUser(req.body.email, code, function(error){
 					if (error) throw error;
 				});
 			}, 2000);
