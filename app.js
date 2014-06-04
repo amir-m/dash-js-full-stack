965569eb3056e6787883229abe5ed02e788682d8
Dev <amir.moravej@gmail.com>
2014-06-04 17:26:26 -0400

    latest

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ setTimeout(function() {
 
 	// require('./helpers')(models, publisher).insertDashesToRedisBackend();
 	// require('./helpers')(models, publisher).deleteAllUsers();
-	require('./helpers')(models, publisher).fixSomething();
+	// require('./helpers')(models, publisher).fixSomething();
 
 }, 3000);
 
@@ -44,7 +44,6 @@ var routes = {
 };
 
 require('./router')(routes, app);
-return;
 
 if (cluster.isMaster) {
 	for (var i = 0; i < cpuCount; i++) {
