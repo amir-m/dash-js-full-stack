292525f534b846fa1d4aa6649be551298842a920
Dev <amir.moravej@gmail.com>
2014-06-12 19:45:40 -0400

    changing schema of private dash

--- app/directive/notifications.js
+++ app/directive/notifications.js
@@ -7,10 +7,10 @@ angular.module('DashbookApp')
     return {
       restrict: 'E',
       replace: true,
-      // templateUrl: '//s3.amazonaws.com/dbk-assets/dash.html', 
       templateUrl: '/partials/notifications.html', 
       link: function (scope, element, attrs) {
+        $(element)
       }
     };
   }
-]);
+  ]);
