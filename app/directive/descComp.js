447bbc19d2b17e429e1f7e9039b7f493b69eff20
Dev <amir.moravej@gmail.com>
2014-06-14 11:55:39 -0400

    changing schema of private dash

--- app/directive/descComp.js
+++ app/directive/descComp.js
@@ -13,8 +13,8 @@ angular.module('DashbookApp')
         scope.content.components.desc_comp.text = scope.content.components.desc_comp.text.replace(/<\/\w*>/g, '');
 
         if (scope.d.title == 'World Cup News') {
-          scope.content.components.desc_comp.header = scope.content.components.desc_comp.header.substr(0, 36);
-          scope.content.components.desc_comp.text = scope.content.components.desc_comp.text.substr(0, 84);
+          // scope.content.components.desc_comp.header = scope.content.components.desc_comp.header.substr(0, 36);
+          // scope.content.components.desc_comp.text = scope.content.components.desc_comp.text.substr(0, 84);
         }
         else {
           if (scope.content.components.desc_comp.header) 
