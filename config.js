e1ddea931f6187f4fc656e1d3cb2f94cd5d380db
Dev <amir.moravej@gmail.com>
2014-06-15 15:53:10 -0400

    latest

--- config.js
+++ config.js
@@ -35,7 +35,7 @@ module.exports = function(express, app, mongoose, cookie, models, redisClient) {
 		req.headers['x-longitude'] = '-73.563195';
 		req.headers['x-userid'] = 'skjhsa28230kldsjsdlkj90834zgtQjE4N0QxNEQzNDUosih3';
 
-		// return next();
+		return next();
 		
 		// if (req.path == '/relaunch' || req.path == '/') {
 		// 	console.log(req.path);
