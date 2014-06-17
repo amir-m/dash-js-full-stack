25eba55565cca5e0f2a6a020b6de348ef8e462a6
Dev <amir.moravej@gmail.com>
2014-06-17 12:43:39 -0400

    latest

--- app/directive/heroComp.js
+++ app/directive/heroComp.js
@@ -8,14 +8,14 @@ angular.module('DashbookApp')
     templateUrl: '/partials/hero-comp.html',
     restrict: 'E',
     link: function postLink(scope, element, attrs) {
-      console.log(scope.d.components_settings)
-      console.log(scope.d.privateDash)
-      return
+      // console.log(scope.d.components_settings)
+      // console.log(scope.d.privateDash)
+      // return
       if (scope.d.components_settings 
         && scope.d.components_settings.hero_comp 
         && scope.d.components_settings.hero_comp.class) {
         for (var i = 0; scope.d.components_settings.hero_comp.class.length; ++i) {
-          $(element).find('.hero-comp > span').addClass(scope.d.components_settings.hero_comp.class[i]);
+          $(element).find('.hero-comp span').addClass(scope.d.components_settings.hero_comp.class[i]);
         }
       };
       scope.$on('suicide', function(){
