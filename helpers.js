60f96675ca6d0244b884d754af81020bb9762e2c
Dev <amir.moravej@gmail.com>
2014-06-23 12:58:20 -0400

    fixing stuff

--- helpers.js
+++ helpers.js
@@ -140,7 +140,7 @@ module.exports = function(models, redisClient) {
 				// 		notifications: 0
 				// 	});
 				// }
-				if (wlz[i].uuid_addaded_at && wlz[i].uuid_addaded_at.length > 0) {
+				if (wlz[i].uuid_addaded_at && wlz[i].uuid_addaded_at.length > 0 && wlz[i].uuid_added_at.length == 0 ) {
 					console.log('uuid_addaded_at: ', wlz[i].uuid_addaded_at);
 					console.log('uuid_added_at: ', wlz[i].uuid_added_at);
 				}
