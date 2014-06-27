31c90722209f36b303225397b4fddca66f02787a
Dev <amir.moravej@gmail.com>
2014-06-26 21:12:34 -0400

    moving dashes back to redis

--- app/directive/dash.js
+++ app/directive/dash.js
@@ -1052,7 +1052,7 @@ angular.module('DashbookApp')
 
           if (scope.d.private_dash.setting_type == 'radio') {
 
-            scope.$watch('scope.currentPoint', function () {
+            scope.$watch('currentPoint', function () {
               console.log('yyyy')
               if (scope.currentPoint == scope.d.content.length - 1 && scope.d.private_dash.settings.length < scope.d.private_dash.settings.indexOf(scope.d.private_dash.selected_setting) + 1) {
                 console.log('should fetch now')
