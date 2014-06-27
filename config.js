15c027ae329fb548ddabdf3746282f3b9ae9a613
Dev <amir.moravej@gmail.com>
2014-06-27 14:39:01 -0400

    moving dashes back to redis

--- config.js
+++ config.js
@@ -36,16 +36,16 @@ module.exports = function(express, app, mongoose, cookie, models, redisClient) {
 
 		// console.log('req.path:');
 
-		res.cookie('uuid', 'assadasdJGHVBVHFR4N0QxNEQzNDUosih3', { maxAge: 100*60*1000, httpOnly: false });
-		res.cookie('sid', 'NTJhYjcwY2M3YjNhNTk3ODYxMDAwMDAx', { maxAge: 100*60*1000, httpOnly: false });
-		res.cookie('latitude', '45.495744', { maxAge: 100*60*1000, httpOnly: false });
-		res.cookie('longitude', '-73.563195', { maxAge: 100*60*1000, httpOnly: false });
+		// res.cookie('uuid', 'assadasdJGHVBVHFR4N0QxNEQzNDUosih3', { maxAge: 100*60*1000, httpOnly: false });
+		// res.cookie('sid', 'NTJhYjcwY2M3YjNhNTk3ODYxMDAwMDAx', { maxAge: 100*60*1000, httpOnly: false });
+		// res.cookie('latitude', '45.495744', { maxAge: 100*60*1000, httpOnly: false });
+		// res.cookie('longitude', '-73.563195', { maxAge: 100*60*1000, httpOnly: false });
 
-		req.headers['x-latitude'] = '45.495744';
-		req.headers['x-longitude'] = '-73.563195';
-		req.headers['x-userid'] = 'assadasdJGHVBVHFR4N0QxNEQzNDUosih3';
+		// req.headers['x-latitude'] = '45.495744';
+		// req.headers['x-longitude'] = '-73.563195';
+		// req.headers['x-userid'] = 'assadasdJGHVBVHFR4N0QxNEQzNDUosih3';
 
-		return next();
+		// return next();
 		
 		// if (req.path == '/relaunch' || req.path == '/') {
 		// 	console.log(req.path);
